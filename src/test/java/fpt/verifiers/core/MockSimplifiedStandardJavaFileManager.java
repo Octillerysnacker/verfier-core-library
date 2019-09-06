33901375759b3bf5b8da9ed670331bacc9bbabe9
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-06 15:52:51 -0500

    added MockSimplifiedStandardJavaFileManager implementtion

--- src/test/java/fpt/verifiers/core/MockSimplifiedStandardJavaFileManager.java
+++ src/test/java/fpt/verifiers/core/MockSimplifiedStandardJavaFileManager.java
@@ -6,10 +6,21 @@ import javax.tools.JavaFileObject;
 
 public class MockSimplifiedStandardJavaFileManager implements ISimplifiedStandardJavaFileManager {
 
+    private Path[] paths;
+    private Iterable<? extends JavaFileObject> getJavaFileObjects_return;
+    public Path[] getSuppliedPaths(){
+        return paths;
+    }
+    public void setReturnForGetJavaFileObjects(Iterable<? extends JavaFileObject> getJavaFileObjects_return){
+        this.getJavaFileObjects_return = getJavaFileObjects_return;
+    }
+    public MockSimplifiedStandardJavaFileManager(Iterable<?extends JavaFileObject> getJavaFileObjects_return){
+        setReturnForGetJavaFileObjects(getJavaFileObjects_return);
+    }
     @Override
     public Iterable<? extends JavaFileObject> getJavaFileObjects(Path... paths) {
-        // TODO Auto-generated method stub
-        return null;
+        this.paths = paths;
+        return getJavaFileObjects_return;
     }
 
 }
