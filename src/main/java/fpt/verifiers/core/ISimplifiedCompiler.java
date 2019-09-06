04640c2ddb4cb949d40440c55637016585b367f4
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-06 15:59:21 -0500

    Removed old compiler file locations

--- src/main/java/fpt/verifiers/core/ISimplifiedCompiler.java
+++
@@ -1,7 +0,0 @@
-package fpt.verifiers.core;
-
-import java.nio.file.Path;
-
-public interface ISimplifiedCompiler {
-    void compileToDir(Path srcDir, Path outDir);
-}
--- src/main/java/fpt/verifiers/core/ISimplifiedStandardJavaFileManager.java
+++
@@ -1,9 +0,0 @@
-package fpt.verifiers.core;
-
-import java.nio.file.Path;
-
-import javax.tools.JavaFileObject;
-
-public interface ISimplifiedStandardJavaFileManager{
-    Iterable<? extends JavaFileObject> getJavaFileObjects(Path... paths);
-}
