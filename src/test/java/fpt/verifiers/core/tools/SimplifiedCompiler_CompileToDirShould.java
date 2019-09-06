924d0151e7a32676c2af254b465bd1acd972a028
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-06 15:58:48 -0500

    Moved to tools package

---
+++ src/test/java/fpt/verifiers/core/tools/SimplifiedCompiler_CompileToDirShould.java
@@ -0,0 +1,16 @@
+package fpt.verifiers.core;
+
+import java.nio.file.Paths;
+
+import javax.tools.StandardJavaFileManager;
+
+import org.junit.jupiter.api.Test;
+
+public class SimplifiedCompiler_CompileToDirShould{
+
+    @Test
+    public void PassCorrectParameters(){
+        
+
+    }
+}
