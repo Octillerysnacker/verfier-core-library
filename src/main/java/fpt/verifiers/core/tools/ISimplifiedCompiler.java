f9d99c3ad2241c04454d8463a34bfc99ef6dba46
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-06 16:16:07 -0500

    Added getSimplifiedCompilationTask to ISimplifiedCompiler signature

--- src/main/java/fpt/verifiers/core/tools/ISimplifiedCompiler.java
+++ src/main/java/fpt/verifiers/core/tools/ISimplifiedCompiler.java
@@ -4,5 +4,10 @@ import javax.tools.DiagnosticListener;
 import javax.tools.JavaFileObject;
 
 public interface ISimplifiedCompiler {
-    ISimplifiedStandardJavaFileManager getSimplifiedStandardJavaFileManager(DiagnosticListener<? super JavaFileObject> diagnosticListener);
+    ISimplifiedStandardJavaFileManager getSimplifiedStandardJavaFileManager(
+            DiagnosticListener<? super JavaFileObject> diagnosticListener);
+
+    ISimplifiedCompilationTask getSimplfiedCompilationTask(ISimplifiedStandardJavaFileManager fileManager,
+            DiagnosticListener<? super JavaFileObject> diagnosticListener, Iterable<String> options,
+            Iterable<? extends JavaFileObject> compilationUnits);
 }
