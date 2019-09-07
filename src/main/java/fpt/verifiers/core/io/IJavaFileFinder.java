4fac7a57d54f0e989c98e28fc7178d1c8f23fb01
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-07 13:40:53 -0500

    Changed IJavaFileFinder signature

--- src/main/java/fpt/verifiers/core/io/IJavaFileFinder.java
+++ src/main/java/fpt/verifiers/core/io/IJavaFileFinder.java
@@ -1,7 +1,8 @@
 package fpt.verifiers.core.io;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public interface IJavaFileFinder{
-    public Path[] getAllFiles(Path srcDirectory);
+    public Path[] getAllFiles(Path srcDirectory) throws IOException;
 }
