ad27132e37fb2ae10799511b061ac122db09d0d9
Octillerysnacker <octillerysnacker@gmail.com>
2019-10-17 12:52:44 -0500

    Added getDefaultDiagnosticCollector to DefaultFPTCompilerFactory

--- src/main/java/fpt/verifiers/core/tools/DefaultFPTCompilerFactory.java
+++ src/main/java/fpt/verifiers/core/tools/DefaultFPTCompilerFactory.java
@@ -1,17 +1,22 @@
 package fpt.verifiers.core.tools;
 
 import javax.tools.DiagnosticCollector;
+import javax.tools.JavaFileObject;
 import javax.tools.ToolProvider;
 
 import fpt.verifiers.core.io.DefaultJavaFileFinder;
 
 public class DefaultFPTCompilerFactory implements IFPTCompilerFactory {
-
+    private static DiagnosticCollector<? super JavaFileObject> collector;
+    public DiagnosticCollector<? super JavaFileObject> getDefaultDiagnosticCollector(){
+        return collector;
+    }
     @Override
     public IFPTCompiler CreateCompiler() {
+        collector = new DiagnosticCollector<>();
         return new FPTCompiler(
             new SimplifiedCompilerWrapper(ToolProvider.getSystemJavaCompiler()),
-            new DiagnosticCollector<>(), 
+            collector, 
             new DefaultJavaFileFinder());
     }
     
