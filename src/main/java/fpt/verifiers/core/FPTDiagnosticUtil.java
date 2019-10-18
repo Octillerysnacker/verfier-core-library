d3c9e274b25779179a1eab6db6c4f4a238ed5127
Octillerysnacker <octillerysnacker@gmail.com>
2019-10-17 22:57:57 -0500

    Added FPTDiagnosticUtil

---
+++ src/main/java/fpt/verifiers/core/FPTDiagnosticUtil.java
@@ -0,0 +1,28 @@
+package fpt.verifiers.core;
+
+import javax.tools.Diagnostic;
+import javax.tools.Diagnostic.Kind;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaFileObject;
+
+public class FPTDiagnosticUtil{
+    public static FPTDiagnostic[] fromDiagnosticCollector(DiagnosticCollector<? extends JavaFileObject> collector){
+        return collector.getDiagnostics().stream().map(FPTDiagnosticUtil::fromDiagnostic).toArray(FPTDiagnostic[]::new);
+    }
+    public static FPTDiagnostic.DiagnosticKind fromJavaDiagnosticKind(Kind kind){
+        if(kind == Kind.ERROR){
+            return FPTDiagnostic.DiagnosticKind.Error;
+        }else if(kind == Kind.WARNING){
+            return FPTDiagnostic.DiagnosticKind.Warning;
+        }else{
+            return FPTDiagnostic.DiagnosticKind.Info;
+        }
+    }
+    public static FPTDiagnostic fromDiagnostic(Diagnostic<? extends JavaFileObject> diagnostic){
+            return new FPTDiagnostic(
+                diagnostic.getMessage(null), 
+                fromJavaDiagnosticKind(diagnostic.getKind()), 
+                new FileLocation(diagnostic.getLineNumber(), diagnostic.getColumnNumber()),
+                null);
+    }
+}
